8548748d6f18e069cb32b3082c4ffef5069d21a9
Aaron Caldwell <arcaldwell49@gmail.com>
2024-08-06 15:46:45 -0500

    adding htest values

--- test_var_diff.R
+++ test_var_diff.R
@@ -35,12 +35,12 @@ p_from_z = function(x,
 }
 
 ## tests ------
-# var.test - F-test
-# barlett bartlett.test()
-# levene car::levene
-# fligner-killeen fligner.test()
-# Breusch-Pagan-Koenker
-# glejser (model based)
+# F-test for variances # var.test()
+# barlett # bartlett.test()
+# levene #  car::levene
+# fligner-killeen # fligner.test()
+# Breusch-Pagan-Koenker # skedastic::breusch_pagan()
+# glejser (model based) # skedastic::glejser()
 glejser.test <- function(model, Z_var, dataset){
   # model is the lmer object
   # Z_var is the explanatory variable to regress the abs values on
@@ -76,7 +76,7 @@ glejser.test <- function(model, Z_var, dataset){
   return(rval)
   
 }
-# difference in variance (normal based test)
+# difference in variance (normal approximation test)
 diff_var_test = function(sd1, n1,
                          sd2, n2,
                          alternative = c("two.sided",
@@ -90,11 +90,75 @@ diff_var_test = function(sd1, n1,
   
   est_diff <- var1-var2
   
-  est_diff_SE <- sqrt(var1^2 + var2^2) 
+  est_diff_SE <- sqrt(var1_se^2 + var2_se^2) 
   teststat <- est_diff/est_diff_SE
-  pvalue = p_from_z(teststat, alternative = alternative)
+  pval = p_from_z(teststat, alternative = alternative)
+  
+  statistic = teststat
+  names(statistic) = "z"
+  null1 = 0
+  names(null1) = "difference in variances"
+  estimate = est_diff
+  names(estimate)= "differnce in variance"
+  std_err = sqrt(est_diff_SE)
+  sum_stats = paste0("SD1 = ", sd1, ", SD2 = ", sd2)
+  
+  rval <- list(statistic = statistic,
+               p.value = as.numeric(pval),
+               stderr = std_err,
+               null.value = null1,
+               alternative = alternative,
+               method = "Difference in Variances",
+               data.name = sum_stats)
+  class(rval) <- "htest"
+  return(rval)
 }
 # SDir (normal based test from Hopkins variances estimate)
+diff_sdir_test = function(sd1, n1,
+                         sd2, n2,
+                         alternative = c("two.sided",
+                                         "greater",
+                                         "less")){
+
+  alternative = match.arg(alternative)
+  var1 = sd1^2
+  var1_se <- var1*(sqrt(2/(n1 - 1)))
+  var2 = sd2^2
+  var2_se <- var2*(sqrt(2/(n2 - 1)))
+  df1 = n1 -1
+  df2 = n2 - 1
+  if((var1-var2)< 0){
+    sd_ir = sqrt(var1-var2)
+    
+  } else{
+    sd_ir = -1*sqrt(var2-var1)
+    
+  }
+  est_diff <- var1-var2
+  
+  est_diff_SE <- sqrt(2 * (sd1^4/df1 + sd2^4/df2)) 
+  teststat <- est_diff/est_diff_SE
+  pval = p_from_z(teststat, alternative = alternative)
+  
+  statistic = teststat
+  names(statistic) = "z"
+  null1 = 0
+  names(null1) = "Standard Deviation of the Individual Response"
+  estimate = sd_ir
+  names(estimate)= "Standard Deviation of the Individual Response"
+  std_err = sqrt(est_diff_SE)
+  sum_stats = paste0("SD1 = ", sd1, ", SD2 = ", sd2)
+  
+  rval <- list(statistic = statistic,
+               p.value = as.numeric(pval),
+               stderr = std_err,
+               null.value = null1,
+               alternative = alternative,
+               method = "Difference in Variances",
+               data.name = sum_stats)
+  class(rval) <- "htest"
+  return(rval)
+}
 # ratio of variances (normal based test?) logVR
 
 log_vr_test = function(sd1, n1,
@@ -102,23 +166,60 @@ log_vr_test = function(sd1, n1,
                        alternative = c("two.sided",
                                        "greater",
                                        "less")){
-  var1 = sd1^2
-  var1_se <- var1*(sqrt(2/(n1 - 1)))
-  var2 = sd2^2
-  var2_se <- var2*(sqrt(2/(n2 - 1)))
-  yi <- log(sqrt(var1)/sqrt(var2)) + 1/(2 * (n1 - 1)) - 1/(2 * 
-                                                                      (n2 - 1))
   
-  vi <- 1/(2 * (n1 - 1)) + 1/(2 * (n2 - 1))
-  
-  teststat <- yi/sqrt(vi)
+  # old code
+  # var1 = sd1^2
+  # var1_se <- var1*(sqrt(2/(n1 - 1)))
+  # var2 = sd2^2
+  # var2_se <- var2*(sqrt(2/(n2 - 1)))
+  # yi <- log(sqrt(var1)/sqrt(var2)) + 1/(2 * (n1 - 1)) - 1/(2 * 
+  #                                                                     (n2 - 1))
+  # 
+  # vi <- 1/(2 * (n1 - 1)) + 1/(2 * (n2 - 1))
+  # 
+  # teststat <- yi/sqrt(vi)
+  alternative = match.arg(alternative)
+  # metafor code
+  es_est = metafor::escalc(
+  measure = "VR",
+  n1i = n1,
+  n2i = n2,
+  sd1i = sd1,
+  sd2i = sd2
+  )
+
+teststat <- es_est$yi/sqrt(es_est$vi)
+pval = p_from_z(teststat, 
+                alternative = alternative)
+
+statistic = teststat
+names(statistic) = "z"
+null1 = 0
+names(null1) = "log ratio of variances"
+estimate = es_est$yi
+names(estimate)= "log VR"
+std_err = sqrt(es_est$vi)
+sum_stats = paste0("SD1 = ", sd1, ", SD2 = ", sd2)
+
+rval <- list(statistic = statistic,
+             p.value = as.numeric(pval),
+             stderr = std_err,
+             null.value = null1,
+             alternative = alternative,
+             method = "log Ratio of Variances",
+             data.name = sum_stats)
+class(rval) <- "htest"
+return(rval)
 }
+
 # coefficient of variation approach
 log_cvr_test = function(sd1, n1, mean1,
                        sd2, n2, mean2,
+                       
                        alternative = c("two.sided",
                                        "greater",
                                        "less")){
+  alternative = match.arg(alternative)
   es_est = metafor::escalc(
      measure = "CVR",
      n1i = n1,
@@ -130,6 +231,27 @@ log_cvr_test = function(sd1, n1, mean1,
   )
   
   teststat <- es_est$yi/sqrt(es_est$vi)
+  pval = p_from_z(teststat, 
+                  alternative = alternative)
+  
+  statistic = teststat
+  names(statistic) = "z"
+  null1 = 0
+  names(null1) = "log ratio of the coefficients of variation"
+  estimate = es_est$yi
+  names(estimate)= "log VR"
+  std_err = sqrt(es_est$vi)
+  sum_stats = paste0("CV1 = ", sd1,"/",mean1, ", CV2 = ", CV2,"/",mean2)
+  
+  rval <- list(statistic = statistic,
+               p.value = as.numeric(pval),
+               stderr = std_err,
+               null.value = null1,
+               alternative = alternative,
+               method = "log Ratio of CVs",
+               data.name = sum_stats)
+  class(rval) <- "htest"
+  return(rval)
 }
 
 
